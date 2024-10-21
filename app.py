294933661e42de3c525206c6b477d8ccd8d58b6f
Phillip Bauer <p.bauer1996@gmx.de>
2024-10-21 11:12:06 +0200

    added the add functionality and route with template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from storage.storage import StorageJson
 import os
 
@@ -8,10 +8,21 @@ file_path = os.path.join("storage", "blog_posts.json")
 storage = StorageJson(file_path)
 
 
-@app.route('/')
+@app.route("/")
 def index():
     blog_posts = storage.list_data()
     return render_template("index.html", posts=blog_posts)
+
+
+@app.route("/add", methods=["POST", "GET"])
+def add():
+    if request.method == "POST":
+        author = request.form.get("author")
+        title = request.form.get("title")
+        content = request.form.get("content")
+        storage.add_data(author, title, content)
+        return redirect(url_for("index"))
+    return render_template("add.html")
 
 
 if __name__ == '__main__':
